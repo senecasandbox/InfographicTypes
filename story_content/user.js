d32dd87cd453f6cba8e996b21acdf853511f7622
senecasandbox <58563392+senecasandbox@users.noreply.github.com>
2021-07-27 16:07:12 -0400

    draft14

--- story_content/user.js
+++ story_content/user.js
@@ -2,6 +2,14 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "6OP95BTLJOb":
+        Script1();
+        break;
   }
+}
+
+function Script1()
+{
+  ga('send', 'screenview', {screenName: 'home'});
 }
 
